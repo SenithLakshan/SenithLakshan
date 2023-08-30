dc7b35f38963c05e63cf80369f8a7d48dc5d1448
Senith Lakshan <143495911+SenithLakshan@users.noreply.github.com>
2023-08-30 20:43:07 +0530

    Add files via upload

---
+++ Week5/w5_t5_Ex1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int num = 10; // Initialize the starting number
+
+    // Use a while loop to Repetition from 10 to 20
+    while (num <= 20) {
+      
+        printf("%d\n", num); // Display the  number
+      
+        num++; // Increment the number for the next Repetition
+      
+    }
+
+    return 0;
+}
